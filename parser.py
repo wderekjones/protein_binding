7463d8e430a887f0fc157d629a4c126420241d9a
Derek <derekjones2025@gmail.com>
2017-02-27 03:13:20 -0500

    starting functions

--- parser.py
+++ parser.py
@@ -8,12 +8,50 @@ import os
     Loading the files in order to determine best utilities. Future improvements will aggregate descriptors for a particular ID and
     output a file ready for training machine learning models.
 '''
-molec_descriptors = pd.read_csv("SampleInputs/MolecularDescriptors/E-DragonOutput7MolecularDescriptors",delimiter='\t',skiprows=2)
 
-data1 = pd.read_csv("SampleInputs/1QCF_cluster1_mmpbsa_energy_avg_5",delimiter='\t')
+path0 = "SampleInputs/MolecularDescriptors/E-DragonOutput7MolecularDescriptors"
 
-data2 = pd.read_csv("SampleInputs/1QCF_docking_results",delimiter='\t')
+path1 = "SampleInputs/1QCF_cluster1_mmpbsa_energy_avg_5"
+
+path2 = "SampleInputs/1QCF_docking_results"
 
 
 
-print data2
+def load_molecular_descriptors(filepath):
+
+	data = pd.read_csv(filepath,delimiter='\t',skiprows=2)
+
+	return data
+
+
+def load_mmgbsa_energy(filepath):
+	data = pd.read_csv(filepath,delimiter='\t')
+
+	return data
+
+def load_docking_results(filepath):
+	data = pd.read_csv(filepath,delimiter='\t')
+	return data
+
+
+'''
+
+	take as input a number of data frames and an identifier. Create a new dataframe from each input dataframe (list of dataframes?) which contains each column for the 
+	identifier. return as numpy array.
+
+def merge_results_on_id():
+	
+
+'''
+
+data0 = load_molecular_descriptors(path0)
+
+
+data1 = load_mmgbsa_energy(path1)
+
+data2 = load_docking_results(path2)
+
+
+
+
+
