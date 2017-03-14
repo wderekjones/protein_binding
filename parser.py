ac8b7c8b8f288ec8a13cfd67fc4bfe028b3c61c7
Derek <derekjones2025@gmail.com>
2017-03-14 16:06:00 -0400

    need to add support to invoke corresponding function to an arbitrary filepath

--- parser.py
+++ parser.py
@@ -91,10 +91,10 @@ def get_merged_results(dfX,dfY,key):
     merge_result = merge_result.set_index(key).reset_index()
     return merge_result
 
-
 #TODO: write a function to merge the molecular descriptors to the docking and energy results. Will need regular expressions.
 #TODO: read the filenames from stdin and then execute necessary functions to generate output datafile
 #TODO: merge all resulting dataframes on id's
+
 
 for filepath in paths:
     d = parse_file(filepath)
