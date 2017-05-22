2810a365a5cb603d4ff44b1de49ce440db952dbd
Derek <derekjones2025@gmail.com>
2017-05-22 17:36:38 -0400

    fixing other small items

--- pd_parser.py
+++ pd_parser.py
@@ -77,9 +77,8 @@ def save_to_hdf5(data_frame):
     data_frame = data_frame.convert_objects(convert_numeric=True)
 
     for feature in data_frame:
-        # feature_list = np.asarray(data_frame[feature].tolist())
-        output_file.create_dataset(str(feature), [data_frame.shape[0], 1], data=data_frame[feature])
-        data_frame.drop([feature], axis=1)
+        feature_list = data_frame.drop([feature],axis=1)
+        output_file.create_dataset(str(feature), [data_frame.shape[0], 1], data=feature_list)
 
     output_file.close()
 
