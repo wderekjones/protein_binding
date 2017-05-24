fc4f19d6bd2e8d92ec36e2b8f190c90956558611
Derek <derekjones2025@gmail.com>
2017-05-24 13:30:12 -0400

    cleaning up the repo

--- test_load_data_csv.py
+++
@@ -1,9 +0,0 @@
-from utils import combine_positive_negative_data
-from utils import load_data_h5
-
-X_p, y_p = load_data_h5("data/ml_pro_features_labels.h5",mode=1)
-X_n, y_n = load_data_h5("data/ml_pro_features_labels.h5",mode=0)
-
-X = combine_positive_negative_data(X_p,X_n)
-y = combine_positive_negative_data(y_p,y_n)
-
